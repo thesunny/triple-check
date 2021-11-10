affd191ab8dd55c26eab2e7274e700801a1eee88
Sunny Hirai <thesunny@gmail.com>
2021-11-09 20:58:38 -0800

    Remove api/check-value

--- pages/api/check-value.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next"
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const json = req.body()
-  console.log("json", json)
-  res.status(200).json({ name: "John Doe" })
-}
